a45fa4a9386857b2523ed973f86eed339a8b5700
Gaby <110936257+Gabylinda2211@users.noreply.github.com>
2022-09-21 15:07:18 -0300

    Add files via upload

--- Cliente1.java
+++ Cliente1.java
@@ -1,14 +1,10 @@
 
 package contabancaria;
 
-/**
- *
- * @author Aluno
- */
 public class Cliente1 {
     public static void main (String[] args){
-            Contabancaria Robson;
-            Robson= new Contabancaria();
+            ContaBancaria Robson;
+            Robson= new ContaBancaria();
             Robson.cliente="Robson Alves";
             Robson.saldo=(2000);
             Robson.limite=(4000);
@@ -16,6 +12,10 @@ public class Cliente1 {
             Robson.numero=(555657);
             Robson.depositar (100);
             Robson.sacar (500);
+            Robson.Quant_parcelas=(5);
+            Robson.emprestar(500);
+            
+            
 }
 }
     
---
+++ Cliente2.java
@@ -0,0 +1,16 @@
+package contabancaria;
+
+public class Cliente2 {
+    public static void main (String[] args){
+            ContaBancaria Gaby;
+            Gaby= new ContaBancaria();
+            Gaby.numero=(4147);
+            Gaby.senha=(18082020);
+            Gaby.cliente= "Gaby Carolina Alves";
+            Gaby.saldo=(2000);
+            Gaby.depositar(500);
+            Gaby.sacar(200);
+         
+            
+}
+}
